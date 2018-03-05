510e2f1fa867427984d0cb89fb9e298cb9014238
Justin Fabricius <jufa2401@gmail.com>
2018-03-05 17:14:17 +0100

    finalized routing for now. Added guards among more

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,6 +2,7 @@ import {Recipe} from './recipe.model';
 import {EventEmitter, Injectable} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
+
 @Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -21,6 +22,10 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(id: number) {
+    return this.recipes[id];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
